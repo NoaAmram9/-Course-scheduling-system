0c5d6fbbe00315641e2321d43426de49c46a1acf
NoaAmram9 <noaamram9421@gmail.com>
2025-04-02 18:51:22 +0300

    improve combination logic

--- SRC/Interfaces/ScheduleService.py
+++ SRC/Interfaces/ScheduleService.py
@@ -1,7 +1,7 @@
 from itertools import product
 from SRC.Interfaces.IScheduleService import IScheduleService
 from SRC.Models.TimeTable import TimeTable
-
+from SRC.Models.Course import Course
 
 class ScheduleService(IScheduleService):
     def generate_schedules(self, courses: list) -> list:
@@ -10,22 +10,25 @@ class ScheduleService(IScheduleService):
         # רשימת כל הקומבינציות האפשריות של שיעורים לכל קורס (הרצאה אחת, תרגול אחד, מעבדה אחת)
         course_combinations = []
         for course in courses:
-            options = []
-            
-            # בחירת אפשרויות להרצאה, תרגול ומעבדה
             lectures = course._lectures if course._lectures else [None]
             exercises = course._exercises if course._exercises else [None]
             labs = course._labs if course._labs else [None]
             
-            # כל שילוב אפשרי של הרצאה אחת, תרגול אחד ומעבדה אחת
-            course_combinations.append(list(product(lectures, exercises, labs)))
+            # כל שילוב אפשרי של הרצאה אחת, תרגול אחד ומעבדה אחת לקורס מסוים
+            course_combinations.append([(course._name, course._code, lec, ex, lab) for lec, ex, lab in product(lectures, exercises, labs)])
         
         # יוצרים את כל הקומבינציות האפשריות של מערכות שעות
         for combination in product(*course_combinations):
-            timetable_lessons = [lesson for course_lessons in combination for lesson in course_lessons if lesson is not None]
+            timetable_courses = []
+            timetable_lessons = []
             
+            for course_name, course_code, lec, ex, lab in combination:
+                selected_lessons = [lesson for lesson in (lec, ex, lab) if lesson is not None]
+                timetable_lessons.extend(selected_lessons)
+                timetable_courses.append(Course(name=course_name, code=course_code, lectures=[lec] if lec else [], exercises=[ex] if ex else [], labs=[lab] if lab else []))
+                
             if self._is_valid_timetable(timetable_lessons):
-                possible_timetables.append(TimeTable(timetable_lessons))
+                possible_timetables.append(TimeTable(timetable_courses))
         
         return possible_timetables
 
