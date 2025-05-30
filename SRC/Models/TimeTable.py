c45310bb10d6544320ef0b806ab08ed0c06b7853
Avigail100 <abigailgor@gmail.com>
2025-05-30 16:10:43 +0300

    Add preferences_details attribute to TimeTable class

--- SRC/Models/TimeTable.py
+++ SRC/Models/TimeTable.py
@@ -1,6 +1,9 @@
+from SRC.Models.Preferences import Preferences
+
 class TimeTable:
-    def __init__(self, courses: list = None):
+    def __init__(self, courses: list = None, preferences_details: Preferences = None):
         self._courses = courses if courses else []  # list of courses in the timetable
+        self._preferences_details = None # Preferences object to hold details for user's displaying preferences
 
     @property
     def courses(self):
@@ -9,3 +12,11 @@ class TimeTable:
     @courses.setter
     def courses(self, value):
         self._courses = value
+        
+    @property
+    def preferences_details(self):
+        return self._preferences_details
+    
+    @preferences_details.setter
+    def preferences_details(self, value: Preferences):
+        self._preferences_details = value
