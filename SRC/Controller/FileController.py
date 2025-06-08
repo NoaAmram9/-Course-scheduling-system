89d8677751c974f53000007dd2474b828c44d0db
Avigail100 <abigailgor@gmail.com>
2025-06-09 01:12:02 +0300

    Refactor file deletion logic to handle multiple file types in a single call

--- SRC/Controller/FileController.py
+++ SRC/Controller/FileController.py
@@ -160,10 +160,11 @@ class FileController:
         delete the temporary files
         
         """
-        file_path1 = self.filePath
-        #file_path2 = "Data/courses.xlsx"
-        file_path2 = "Data/selected_courses.txt"
+        # file_path1 = self.filePath
+        # file_path2 = "Data/courses.xlsx"
+        # file_path2 = "Data/selected_courses.txt"
+        files = ["Data/courses.xlsx", "Data/selected_courses.txt", self.filePath]
         dataManager = FileManager()
-        dataManager.delete_temp_files(file_path1, file_path2)
+        dataManager.delete_temp_files(files)
 
     
